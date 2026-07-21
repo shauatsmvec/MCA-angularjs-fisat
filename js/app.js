d20f81ce6d2447ff3ccc573f04fab8097316fcb4
shauatsmvec <shaukhan8082004@gmail.com>
2026-07-21 12:17:02 +0530

    ceated the app.js and used directives and data binding

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app =angular.module('campus360',[]);
+
+app.controller("dashboardcontroller",function($scope){
+
+
+    $scope.collegename="FISAT";
+     $scope.totalStudents = 1200;
+    $scope.totalFaculty = 85;
+    console.log("dashboard controller is loaded");
+
+    $scope.students=[
+        "shahid","jhon","allen","shaun"
+    ];    
+    
+   $scope.addStudent = function(){
+
+    $scope.totalStudents++;
+
+};
+});
+
