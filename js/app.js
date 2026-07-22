ec9cab9016f3986e03fd54c18936f0adb60150b8
shauatsmvec <shaukhan8082004@gmail.com>
2026-07-22 09:36:35 +0530

    ng-class and ng-style correct code

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ app.controller("dashboardcontroller",function($scope){
 
     $scope.collegename="FISAT";
      $scope.totalStudents = 1200;
-    $scope.totalFaculty = 85;
+    $scope.totalFaculty = 120;
     console.log("dashboard controller is loaded");
 
     $scope.students=[
@@ -16,6 +16,11 @@ app.controller("dashboardcontroller",function($scope){
 
     $scope.totalStudents++;
 
+    $scope.showstudents=true;
+
+    $scope.showDashboard = true;
+    
+    
 };
 });
 
